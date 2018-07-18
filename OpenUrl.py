fd6218636f8105396337ce354f3d4454f35ce136
Yasothai <yasojcetcse@gmail.comqexitgit config --global user.email yasojcetcse@gmail.com>
2018-07-18 22:47:22 +0200

    PySeleniumUrlOpen

---
+++ OpenUrl.py
@@ -0,0 +1,10 @@
+import unittest
+from selenium import webdriver
+
+driver = webdriver.Chrome("C:\\Selenium\\Chrome\\chromedriver.exe")
+driver.set_page_load_timeout(20)
+driver.get("http://google.com")
+driver.maximize_window()
+driver.implicitly_wait(20)
+driver.close()
+driver.quit()
